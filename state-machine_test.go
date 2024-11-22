016b43943e275a15faf3cc0ffe563bc81d1048be
Yannick Kirschen <yannickkirschen@protonmail.com>
2024-11-22 15:22:26 +0100

    chore: state constants in unit tests

--- state-machine_test.go
+++ state-machine_test.go
@@ -5,10 +5,18 @@ import (
 	"testing"
 )
 
+const (
+	Open  = "open"
+	Close = "close"
+
+	OpenDoor  = "open-door"
+	CloseDoor = "close-door"
+)
+
 func getMachine() *Machine {
-	machine := NewMachine("close")
-	machine.SetTransition("open", "close-door", "close")
-	machine.SetTransition("close", "open-door", "open")
+	machine := NewMachine(Close)
+	machine.SetTransition(Open, CloseDoor, Close)
+	machine.SetTransition(Close, OpenDoor, Open)
 
 	machine.SetEnterAction(func(last, new State) error {
 		fmt.Printf("Enter '%s' coming from '%s'\n", new, last)
@@ -26,13 +34,13 @@ func getMachine() *Machine {
 func TestState(t *testing.T) {
 	machine := getMachine()
 
-	if machine.State() != "close" {
+	if machine.State() != Close {
 		t.Error("expecting state to be close")
 	}
 
-	machine.Transition("open-door")
+	machine.Transition(OpenDoor)
 
-	if machine.State() != "open" {
+	if machine.State() != Open {
 		t.Error("expecting state to be open")
 	}
 }
@@ -40,11 +48,11 @@ func TestState(t *testing.T) {
 func TestTransition(t *testing.T) {
 	machine := getMachine()
 
-	if err := machine.Transition("open-door"); err != nil {
+	if err := machine.Transition(OpenDoor); err != nil {
 		t.Error(err)
 	}
 
-	if err := machine.Transition("open-door"); err == nil {
+	if err := machine.Transition(OpenDoor); err == nil {
 		t.Error("expecting door to be opened")
 	}
 }
@@ -52,11 +60,11 @@ func TestTransition(t *testing.T) {
 func TestCanTransition(t *testing.T) {
 	machine := getMachine()
 
-	if !machine.CanTransition("open-door") {
+	if !machine.CanTransition(OpenDoor) {
 		t.Error("expecting machine.CanTransition(\"open-door\") to return true")
 	}
 
-	if machine.CanTransition("close-door") {
+	if machine.CanTransition(CloseDoor) {
 		t.Error("expecting machine.CanTransition(\"close-door\") to return false")
 	}
 }
